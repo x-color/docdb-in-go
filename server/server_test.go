412ec373e0aa2fd9fe56a8b6386395dcf47910aa
x-color <36035885+x-color@users.noreply.github.com>
2022-06-18 20:15:41 +0900

    Fix TestServer_SearchDocumentHandler

--- server/server_test.go
+++ server/server_test.go
@@ -2,12 +2,16 @@ package server
 
 import (
 	"bytes"
+	"crypto/sha256"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"sort"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"github.com/gorilla/mux"
 	"github.com/x-color/docdb-in-go/docdb"
 )
@@ -53,7 +57,7 @@ func TestServer_AddDocumentHandler(t *testing.T) {
 			router.ServeHTTP(rr, req)
 
 			if rr.Code != tt.wantCode {
-				t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, tt.code)
+				t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, tt.wantCode)
 			}
 
 			if rr.Code != http.StatusCreated {
@@ -129,7 +133,7 @@ func TestServer_GetDocumentHandler(t *testing.T) {
 			router.ServeHTTP(rr, req)
 
 			if rr.Code != tt.wantCode {
-				t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, tt.code)
+				t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, tt.wantCode)
 			}
 
 			if rr.Code != http.StatusOK {
@@ -152,21 +156,76 @@ func TestServer_SearchDocumentHandler(t *testing.T) {
 	tests := []struct {
 		name     string
 		server   Server
+		docs     []map[string]any
 		q        string
 		wantCode int
-		wantDoc  map[string]any
+		wantRes  map[string]any
 	}{
 		{
 			name: "Search document",
 			server: Server{
 				docdb: docdb.NewDocDB(),
 			},
-			q:        "greeting:hello",
-			wantCode: http.StatusOK,
-			wantDoc: map[string]any{
-				"document": map[string]any{
+			docs: []map[string]any{
+				{
+					"greeting": "hello",
+					"obj": map[string]any{
+						"num": "1",
+					},
+				},
+				{
 					"greeting": "hello",
 				},
+			},
+			q:        "obj.num:1",
+			wantCode: http.StatusOK,
+			wantRes: map[string]any{
+				"documents": []any{
+					map[string]any{
+						"document": map[string]any{
+							"greeting": "hello",
+							"obj": map[string]any{
+								"num": "1",
+							},
+						},
+					},
+				},
+				"count": float64(1),
+			},
+		},
+		{
+			name: "Search documents",
+			server: Server{
+				docdb: docdb.NewDocDB(),
+			},
+			docs: []map[string]any{
+				{
+					"greeting": "hello",
+					"num":      1,
+				},
+				{
+					"greeting": "hello",
+					"num":      2,
+				},
+			},
+			q:        "greeting:hello",
+			wantCode: http.StatusOK,
+			wantRes: map[string]any{
+				"documents": []any{
+					map[string]any{
+						"document": map[string]any{
+							"greeting": "hello",
+							"num":      float64(1),
+						},
+					},
+					map[string]any{
+						"document": map[string]any{
+							"greeting": "hello",
+							"num":      float64(2),
+						},
+					},
+				},
+				"count": float64(2),
 			},
 		},
 		{
@@ -174,21 +233,29 @@ func TestServer_SearchDocumentHandler(t *testing.T) {
 			server: Server{
 				docdb: docdb.NewDocDB(),
 			},
+			docs: []map[string]any{
+				{
+					"greeting": "hello",
+					"num":      1,
+				},
+				{
+					"greeting": "hello",
+					"num":      2,
+				},
+			},
 			q:        "a.b.:1",
 			wantCode: http.StatusNotFound,
-			wantDoc:  make(map[string]any),
+			wantRes:  nil,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			testdata := map[string]any{
-				"greeting": "hello",
+			for _, doc := range tt.docs {
+				_, err := tt.server.docdb.Add(doc)
+				if err != nil {
+					t.Fatalf("failed to add data to DB for preparing test: %v", err)
+				}
 			}
-			id, err := tt.server.docdb.Add(testdata)
-			if err != nil {
-				t.Fatalf("failed to add data to DB for preparing test: %v", err)
-			}
-			tt.wantDoc["id"] = id
 
 			req, err := http.NewRequest("GET", "/docs?q="+tt.q, nil)
 			if err != nil {
@@ -201,7 +268,7 @@ func TestServer_SearchDocumentHandler(t *testing.T) {
 			router.ServeHTTP(rr, req)
 
 			if rr.Code != tt.wantCode {
-				t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, tt.code)
+				t.Errorf("handler returned wrong status code: got %v want %v", rr.Code, tt.wantCode)
 			}
 
 			if rr.Code != http.StatusOK {
@@ -213,7 +280,26 @@ func TestServer_SearchDocumentHandler(t *testing.T) {
 				t.Errorf("handler returned invalid body: got %v", rr.Body.String())
 			}
 
-			if diff := cmp.Diff(tt.wantDoc, res); diff != "" {
+			sortOpt := cmp.Transformer("sort", func(in map[string]any) map[string]any {
+				docs, ok := in["documents"].([]any)
+				if !ok {
+					return in
+				}
+				sort.Slice(docs, func(i, j int) bool {
+					hi := fmt.Sprintf("%s", sha256.Sum256([]byte(fmt.Sprintf("%v", docs[i]))))
+					hj := fmt.Sprintf("%s", sha256.Sum256([]byte(fmt.Sprintf("%v", docs[j]))))
+					return hi < hj
+				})
+				return map[string]any{
+					"documents": docs,
+					"count":     in["count"],
+				}
+			})
+			ignoreOpt := cmpopts.IgnoreMapEntries(func(k, v any) bool {
+				return k == "id"
+			})
+
+			if diff := cmp.Diff(tt.wantRes, res, sortOpt, ignoreOpt); diff != "" {
 				t.Errorf("document mismatch (-want +got):\n%s", diff)
 			}
 		})
